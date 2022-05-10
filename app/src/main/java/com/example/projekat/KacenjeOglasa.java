a6d218e51688a276c26b677c60cb582193c3e10e
DavidTot <63733844+DavidTot@users.noreply.github.com>
2022-05-10 23:24:25 +0200

    kacenjeoglasa

--- app/src/main/java/com/example/projekat/KacenjeOglasa.java
+++ app/src/main/java/com/example/projekat/KacenjeOglasa.java
@@ -3,6 +3,7 @@ package com.example.projekat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -35,9 +36,41 @@ public class KacenjeOglasa extends AppCompatActivity {
     protected void postavi() {
         new Thread(() -> {
 
-            JSONObject json = new JSONObject();
+            EditText imeprezimeet = findViewById(R.id.imeiprezime);
+            String imeprezime = imeprezimeet.getText().toString();
+            EditText datumrodjenjaet = findViewById(R.id.datumrodjenja);
+            String datumrodjenja = datumrodjenjaet.getText().toString();
+            EditText mestorodjenjaet = findViewById(R.id.mestorodjenja);
+            String mestorodjenja = mestorodjenjaet.getText().toString();
+            EditText ocenaradnesposobnostiet = findViewById(R.id.ocenaradnesposobnosti);
+            String ocenaradnesposobnosti = ocenaradnesposobnostiet.getText().toString();
+            EditText radnoiskustvoet = findViewById(R.id.radnoiskustvo);
+            String radnoiskustvo = radnoiskustvoet.getText().toString();
+            EditText dodatnesposobnostiet = findViewById(R.id.dodatnesposobnosti);
+            String dodatnesposobnosti = dodatnesposobnostiet.getText().toString();
+            EditText cenaet = findViewById(R.id.cena);
+            String cena = cenaet.getText().toString();
 
-
+            JSONObject objekat = new JSONObject();
+            JSONObject rodjenje = new JSONObject();
+            try {
+                objekat.put("naziv", imeprezime);
+                rodjenje.put("datum", datumrodjenja);
+                rodjenje.put("mesto", mestorodjenja);
+                objekat.put("rodjenje", rodjenje);
+                objekat.put("ocena", ocenaradnesposobnosti);
+                objekat.put("radnoIskustvo", radnoiskustvo);
+                objekat.put("dodatneSposobnosti", dodatnesposobnosti);
+                objekat.put("cena", cena);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String URL= URLserver+"/api/radnici";
+            try {
+                httpHelper.postJSONObjectFromURL(URL,objekat);
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
         }).start();
     }
 }
